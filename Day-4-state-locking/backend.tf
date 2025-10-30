d0c9c128468edb74b9a6385f86995cde6b1891cb
rajashri <rajashrigahane2018@gmail.com>
2025-10-30 17:33:51 +0530

    updated

---
+++ Day-4-state-locking/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "myterraformbucketstatelocking"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    #use_lockfile   = true # Enables S3 native state locking
+  }
+}

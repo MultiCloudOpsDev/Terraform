467d22055a51be690d63de5d82d50832a8f1bde7
rajashri <rajashrigahane2018@gmail.com>
2025-10-30 18:23:15 +0530

    upd

--- Day-4-state-locking/backend.tf
+++ Day-4-state-locking/backend.tf
@@ -4,7 +4,7 @@ terraform {
     key            = "terraform.tfstate"
     region         = "us-east-1"
     #use_lockfile   = true # Enables S3 native state locking(versions of Terraform(e.g., 1.9 and later))
-    dynamodb_table = "" # any version we can use dynamodb state locking (DynamoDB table must have a partition key named LockID)
+    dynamodb_table = "terraform-state-locking" #add dynamodb table name ->any version we can use dynamodb state locking (DynamoDB table must have a partition key named LockID)
     encrypt = true
   }
 }
